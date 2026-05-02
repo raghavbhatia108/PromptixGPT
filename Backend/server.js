1d40955fe9744fb9e8731f01b706346b2e1fe460
Raghav bhatia <bhatiaraghav70@gmail.com>
2026-05-02 18:10:03 +0530

    Project ready for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -11,11 +11,22 @@ const app =express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-const allowedOrigins = process.env.FRONTEND_URL
-  ? [process.env.FRONTEND_URL]
-  : true;
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+];
 
-app.use(cors({ origin: allowedOrigins }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 app.use((req, res, next) => {
   console.log(`[Backend Request] ${req.method} ${req.originalUrl}`);
